329594c0e1c0b85c2d8c30dd1e03dc83204aa9e6
Alexuva <alejandrohernandezrosa@gmail.com>
2022-09-06 19:35:09 +0200

    agregadas imágenes a la búsqueda

--- search.py
+++ search.py
@@ -1,11 +1,11 @@
 from requests_html import HTMLSession
 
 class Search():
-
     def searchdata(self, title):
         url = f"https://gocdkeys.com/es/buscar/all/pc-cd-key/all?product={title}&order=&program=&tags="
         s = HTMLSession()
         r = s.get(url)
+        enlace_original = "https://gocdkeys.com"
         print(r.status_code)
 
         busqueda = r.html.find("div.result-items", first=True)
@@ -13,15 +13,21 @@ class Search():
         searchList = []
 
         for resultado in resultados:
-            anchor = resultado.find("h2.media-heading.truncate > a")
-            for title in anchor:
+            titulo = resultado.find("h2.media-heading.truncate > a")
+            img = resultado.find("img.media-object")
+            for imagen in img:
+                image = enlace_original + imagen.attrs["src"]
+            for title in titulo:
                 enlace = str(title.absolute_links).split("es/")
                 nuevo_enlace = "https://game-price.herokuapp.com/"+ enlace[1]
                 nuevo_nuevo_enlace = nuevo_enlace.replace("'}", "")
-                game = {
-                    "Titulo": f"{title.text}",
-                    "Enlace": f"{nuevo_nuevo_enlace}"
-                }
-                searchList.append(game)
+
+            game = {
+                "Titulo": f"{title.text}",
+                "Enlace": f"{nuevo_nuevo_enlace}",
+                "Caratula": f"{image}"
+            }
+
+            searchList.append(game)
 
         return searchList
